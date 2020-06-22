72f2a8fdad612f96589d75aae05d5dbd91fcbba1
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-22 19:57:41 +0500

    update some task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import React, {useState} from 'react';
-import {StyleSheet,Text, View,FlatList} from 'react-native';
+import React from 'react';
+import Main from './Main';
 
-export default function App() {
-    const [todos, setTodos]=useState([
-        {Text: 'buy coffee',key: '1'},
-        {Text: 'create an app',key: '2'},
-        {Text: 'play on the switch',key: '3'}
-
-    ]);
-  return (
-      <View Style={Styles.container}>
-      {/*header*/}
-      <View Style={Styles.content}>
-      {/*to from*/}
-      <View Style={Styles.list}>
-      <FlatList
-      data={todos}
-      renderItem{({item})} => (
-      )}
-      />
-      </View></View>
-      </View>
-  )
-
+export default class Main React {
+  
 }
