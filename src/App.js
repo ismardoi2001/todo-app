dd4a3939d227f76f3dcbb1bd2bbfae15a5a7d8b7
Muhammad Ismail <ismardoi2001@gmail.com>
2020-06-23 13:58:00 +0500

    update task

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 const App =() => {
     return <>
     <div className="main_div">
-    <div>
+    <div className ="center_div">
+    <br/>
+    <h1> ToDo List</h1>
     </div>
 
     </div>
